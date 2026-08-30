12a23d37a7b1a669cbe2f1ee4280a0822fb00af2
Satyamcode11 <moharanasatyam11@gmail.com>
2026-08-30 21:22:05 +0530

    Learn and Practice Synchronization in Selenium

--- SeleniumWebdriver/src/main/java/cwh_008_Synchronization.java
+++ SeleniumWebdriver/src/main/java/cwh_008_Synchronization.java
@@ -4,16 +4,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class cwh_008_Synchronization {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/loginpagePractise/");
@@ -29,7 +27,7 @@ public class cwh_008_Synchronization {
         loginPage(driver, userName, password, wait, element, items);
     }
 
-    public static void loginPage(WebDriver driver, String userName, String password, WebDriverWait wait, WebElement element, String[] items) throws InterruptedException {
+    public static void loginPage(WebDriver driver, String userName, String password, WebDriverWait wait, WebElement element, String[] items) {
         int j = 0;
         driver.findElement(By.id("username")).sendKeys(userName);
         driver.findElement(By.id("password")).sendKeys(password);
